a0cbd7b198285442214be4fb0f6a2bb009aa4ee0
UJ-SIAO <a2051210080@gmail.com>
2021-07-15 22:02:09 +0800

    update code

--- ImageProcessing.srcs/sources_1/new/conv1.v
+++ ImageProcessing.srcs/sources_1/new/conv1.v
@@ -30,8 +30,6 @@ output reg   o_convolved_data_valid
 
 integer i; 
 
-reg [7:0] kernel1 [8:0];
-
 reg [15:0]stage1_data[8:0];
 reg [7:0] i_pixel_data_light_s1[8:0];
 reg [7:0] i_pixel_data_light_s1_threshold[8:0];
@@ -41,11 +39,9 @@ reg [7:0] i_pixel_data_light_s3[8:0];
 reg [7:0] multData1[8:0];
 reg [7:0] multData2[8:0];
 reg [7:0] multData3[8:0];
-reg [7:0] multData1_s2;
 reg [7:0] sumDataInt2[8:0];
 reg [15:0] compared_data[8:0];
 reg [7:0] lonely_data[7:0];
-reg [7:0] reserved_data[7:0];
 reg [7:0] multData1_s3;
 reg [7:0] multData1_s4;
 reg [7:0] multData1_s5;
@@ -62,30 +58,11 @@ reg curved_Vaild;
 reg multDataValid;
 reg sumDataInt2_vaild;
 reg sumDataValid;
-reg convolved_data_valid;
 reg convolved_data_int_valid;
 reg compared_data_valid;
 reg compared_data_valid_s2;
 reg dodged_data_valid_s1;
-reg dodged_data_valid_s2;
-
-/*reg [7:0] sumData2;
-reg [7:0]testData;
-reg [7:0]testData3;
-reg [7:0]testData2[8:0];*/
-
-initial
-begin
-    kernel1[0] =  0;
-    kernel1[1] =  0;
-    kernel1[2] =  0;
-    kernel1[3] =  0;
-    kernel1[4] =  1;
-    kernel1[5] =  0;
-    kernel1[6] =  0;
-    kernel1[7] =  0;
-    kernel1[8] =  0;
-end    
+reg dodged_data_valid_s2;   
 
 always @(posedge i_clk)
 begin
@@ -111,8 +88,7 @@ begin
 	begin
 		i_pixel_data_light_s1_threshold[i] <= i_pixel_data_light_s1[i]-20;
 		i_pixel_data_light_s2[i] <= i_pixel_data_light_s1[i];
-		//for test
-		//sumDataInt2[i] = multData2[i];
+
 		threshold_vaild<= lightDataValid;
 	end
 end
@@ -131,18 +107,9 @@ always @(posedge i_clk)
 begin
 	for(i=0;i<9;i=i+1)
 	begin
-		//multData2[i] <= (i_pixel_data_light[i] < 40) ?  8'b0  : ((i_pixel_data_light[i] - 40) * 42 ) / 43;	//(40,0)->(255,210)
 		multData2[i] <= (i_pixel_data_light_s3[i] > 20) ?  curved_data[i]  : 8'b0;		//(40,0)->(255,230)
-		//multData2[i] <= (i_pixel_data_light[i] < 40) ?  8'd40 : ((i_pixel_data_light[i] - 40) * 34 ) / 43;	//(40,40)->(255,210)
-		//multData2[i] <= (i_pixel_data_light[i] < 40) ?  8'b0  : ((i_pixel_data_light[i] - 40) * 51 ) / 43;	//(40,0)->(255,255)
-		//multData2[i] <= (i_pixel_data_light[i] < 40) ?  8'd40 : ((i_pixel_data_light[i] - 40) * 51 ) / 43;	//(40,40)->(255,255)
-		//multData2[i] <= (i_pixel_data_light[i] < 70) ?  8'd0 : ((i_pixel_data_light[i] - 70) * 42 ) / 37;		//(70,0)->(255,210)
-		//multData2[i] <= (i_pixel_data_light[i] < 70) ?  8'd0 : ((i_pixel_data_light[i] - 70) * 46 ) / 37;		//(70,0)->(255,230)
-
-		//for test
-		//multData2[i] <= i_pixel_data_light[i];
-
 	end
+	
 	multDataValid <= curved_Vaild;
 end
 
@@ -153,8 +120,6 @@ begin
 	begin
 		sumDataInt2[i] <= 255-multData2[i];
 		multData3[i] <= multData2[i];
-		//for test
-		//sumDataInt2[i] = multData2[i];
 	end
 
 	sumDataInt2_vaild<= multDataValid;
@@ -167,76 +132,19 @@ begin
 	compared_data[2] <= (sumDataInt2[4] <= sumDataInt2[5]) ? sumDataInt2[4] : sumDataInt2[5];
 	compared_data[3] <= (sumDataInt2[6] <= sumDataInt2[7]) ? sumDataInt2[6] : sumDataInt2[7];
 	lonely_data[0]   <= sumDataInt2[8];
-	/*for(i=0;i<9;i=i+1)
-	begin
-		//testData2[i] <= $signed(kernel1[i]) * $signed({1'b0,multData2[i]});
-		//multData1[i] <= $signed(kernel1[i]) * $signed({1'b0,multData2[i]});
-		//for test
-		//multData1[i] <= $signed(kernel1[i]) * $signed({1'b0,sumDataInt2[i]});
-	end*/
 	multData1[4] <= multData3[4];
+	
 	sumDataValid <= sumDataInt2_vaild;
 end
-
-/*always @(posedge i_clk) //4.5.6.7 reserved
-begin
-	compared_data[0] <= (sumDataInt2[0] <= sumDataInt2[1]) ? sumDataInt2[0] : sumDataInt2[1];
-	compared_data[1] <= (sumDataInt2[2] <= sumDataInt2[3]) ? sumDataInt2[2] : sumDataInt2[3];
-	
-	reserved_data[4] <= sumDataInt2[4];
-	reserved_data[5] <= sumDataInt2[5];
-	reserved_data[6] <= sumDataInt2[6];
-	reserved_data[7] <= sumDataInt2[7];
-	
-	compared_data[4] <= sumDataInt2[8];
-	for(i=0;i<9;i=i+1)
-	begin
-		//testData2[i] <= $signed(kernel1[i]) * $signed({1'b0,multData2[i]});
-		//multData1[i] <= $signed(kernel1[i]) * $signed({1'b0,multData2[i]});
-		//for test
-		multData1[i] <= $signed(kernel1[i]) * $signed({1'b0,sumDataInt2[i]});
-	end
-	sumDataValid <= multDataValid;
-end*/
-
-/*always @(posedge i_clk) //0.1.2.3 reserved
-begin
-	reserved_data[0] <= compared_data[0];
-	reserved_data[1] <= compared_data[1];
-	compared_data[2] <= (reserved_data[4] <= reserved_data[5]) ? reserved_data[4] : reserved_data[5];
-	compared_data[3] <= (reserved_data[6] <= reserved_data[7]) ? reserved_data[6] : reserved_data[7];
-	reserved_data[2] <= compared_data[4];
-	for(i=0;i<9;i=i+1)
-	begin
-		//testData2[i] <= $signed(kernel1[i]) * $signed({1'b0,multData2[i]});
-		//multData1[i] <= $signed(kernel1[i]) * $signed({1'b0,multData2[i]});
-		//for test
-		multData1[i] <= $signed(kernel1[i]) * $signed({1'b0,sumDataInt2[i]});
-	end
-	sumDataValid <= multDataValid;
-end*/
-
-/*always @(*)
-begin
-	//testData3=0;
-	multData1_s2 = 0;
-	for(i=0;i<9;i=i+1)
-	begin
-		multData1_s2 = multData1_s2 + multData1[i];
-		//testData3 = testData3 + $signed(testData2[i]);
-	end
-end*/
 
 always @(posedge i_clk)
 begin
 	compared_data[5] <= (compared_data[0] <= compared_data[1]) ? compared_data[0] : compared_data[1];
 	compared_data[6] <= (compared_data[2] <= compared_data[3]) ? compared_data[2] : compared_data[3];
 	lonely_data[1]   <= lonely_data[0];
-	//sumData2<=testData3;
 	multData1_s3<=multData1[4];
+	
     convolved_data_int_valid <= sumDataValid;
-	//for test
-	//reserved_data[3] <= reserved_data[2];
 end
 
 always @(posedge i_clk)
@@ -263,8 +171,6 @@ begin
 	dodged_data_valid_s1 <= compared_data_valid_s2;
 end
 
-//assign	inverse_multData1_s6 = 255 - multData1_s6;
-
 always @(posedge i_clk)
 begin
 	dodge_data_s2 <= dodge_data_s1 / inverse_multData1_s6;
@@ -275,26 +181,9 @@ end
 
 always @(posedge i_clk)
 begin
-	/*o_convolved_data <= (multData1_s5 >= 200 ) ? 8'd255 : 
-						(255 < (picture1 / picture2)) ?  8'd255 : (picture1 / picture2);*/
-	/*o_convolved_data <= (multData1_s5 == 255 ) ? 8'd255 : 
-						(255 < ((compared_data[8] * 255) / (255 - multData1_s5))) ?  8'd255 : ((compared_data[8] * 255) / (255 - multData1_s5));*/
 	o_convolved_data <= (multData1_s7 == 255 ) ? 8'd255 : 
 							(15'd255 > dodge_data_s2) ? dodge_data_s2 : 8'd255;
 
-	/*if(multData1_s7 == 8'd255)
-	begin
-		o_convolved_data <= 8'd255;
-	end
-	else begin
-		if(8'd255 > dodge_data_s2)begin
-			o_convolved_data <= dodge_data_s2;
-		end
-		else begin
-			o_convolved_data <= 8'd255;
-		end
-	end*/
-	//o_convolved_data<=compared_data[8];
 	o_convolved_data_valid <= dodged_data_valid_s2;
 end
 
